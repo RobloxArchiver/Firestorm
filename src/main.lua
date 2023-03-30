8c1dffaf824d9b25f8f8164a9dce7ac9ba91a7dc
Archiver <RobloxArchiver@protonmail.com>
2023-03-30 03:26:58 -0400

    cdef for textbox

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,11 @@
-local ENV = getfenv()
+local ffi = require("ffi")
 
-for i, v in pairs(ENV) do
-    print(i, v)
+ffi.cdef[[
+    int MessageBoxA(void *w, const char *txt, const char *cap, int type);
+]]
+
+function TextBox(text, title)
+    ffi.C.MessageBoxA(nil, text, title, 0)
 end
+
+TextBox("Hail Hydra", " 🙏 ")
