0aee32451d3cc3144a9e374e2c04a608b811d2d9
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-03-31 08:01:31 -0400

    Create data.lua

---
+++ src/winuser/data.lua
@@ -0,0 +1,13 @@
+export = {}
+
+export.MessageBoxButtons = {
+    ["MB_ABORTRETRYIGNORE"] = 0x00000002L,
+    ["MB_CANCELTRYCONTINUE"] = 0x00000006L,
+    ["MB_HELP"] = 0x00004000L,
+    ["MB_OK"] = 0x00000000L,
+    ["MB_OKCANCEL"] = 0x00000005L,
+    ["MB_YESNO"] = 0x00000004L,
+    ["MB_YESNOCANCEL"] = 0x00000003L
+}
+
+return export
