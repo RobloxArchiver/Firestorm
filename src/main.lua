dba5a7846ec3ccf113e8ae482d216c3a96464f3c
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-03-31 10:02:13 -0400

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -4,7 +4,10 @@
 
     1: Simple Textbased Adventure Game "Framework"
     2: Keeping it all to one file, or at least just one library
+        2a: Framework idea is easier
     3: Using LuaJIT to an advantage
     4: ZeroBraneStudio Debugging and Support
 
 ]]
+
+local Firestorm = {}
