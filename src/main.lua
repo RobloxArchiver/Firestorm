2c5fc1a7f753c9385b5cae9e547014f5469a1128
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2023-03-31 10:14:40 -0400

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -10,4 +10,38 @@
 
 ]]
 
-local Firestorm = {}
+firestorm = {}
+
+firestorm.defaultPath = "./firestorm_game_data/"
+
+firestorm.createGame(_self)
+    local ret = {}
+    
+    ret.gameData = {
+        name = _self.name or "Firestorm",
+        version = _self.version or "0.0.0",
+        savePath = _self.savePath or firestorm.defaultPath
+    }
+    
+    function ret.GameName()
+        return ret.name
+    end
+    
+    function ret.GameVersion()
+        return ret.version
+    end
+    
+    function ret.getSavePath()
+        return ret.savePath
+    end
+    
+    return ret
+end
+
+local newGame = firestorm.createGame({gameName = "TEST", gameVersion = "v6.9.6.9", "./hello_world"})
+
+for i, v in pairs(newGame) do
+    if type(v) == "function" then
+        print(v())
+    end
+end
